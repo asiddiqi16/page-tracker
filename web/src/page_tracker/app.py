3c9d84c0f4105a21a2ef74474e3c37526f1ddd09
asiddiqi16 <ariba.siddiqi@gmail.com>
2025-05-16 20:02:29 +1000

    refactor folders and created docker compose

---
+++ web/src/page_tracker/app.py
@@ -0,0 +1,27 @@
+# src/page_tracker/app.py
+"""This is a test app."""
+import os
+from functools import cache
+
+from flask import Flask
+from redis import Redis, RedisError
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    """Root Index"""
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")
+        return "Sorry, something went wrong \N{PENSIVE FACE}", 500
+    else:
+        return f"This page has been seen {page_views} times."
+
+
+@cache
+def redis():
+    """Return a redis client."""
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
